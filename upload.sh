ba48a91ef42a2f43144a7b26409ed89674714ac4
fajtai_andris <andris.fajkusz@gmail.com>
2020-09-08 20:53:32 +0200

    Using Arduino-cli again instead of AVRDUDE

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 arduino-cli compile --fqbn arduino:avr:uno . &&
-avrdude -v -V -patmega328p -carduino -P/dev/ttyUSB0 -D -Uflash:w:./build/arduino.avr.uno/code.ino.hex:i
+mv ./build/arduino.avr.uno/code.ino.hex ./build/arduino.avr.uno/..ino.hex &&
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno .

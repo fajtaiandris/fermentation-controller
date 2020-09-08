684613288799d40bfc2771eaaa8e47d8c413864c
Fajtai Andris <andris.fajkusz@gmail.com>
2020-09-08 17:26:51 +0200

    Using AVRDUDE for upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,2 @@
 arduino-cli compile --fqbn arduino:avr:uno . &&
-mv ./build/arduino.avr.uno/code.ino.hex ./build/arduino.avr.uno/..ino.hex &&
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno .
-
+avrdude -v -V -patmega328p -carduino -P/dev/ttyUSB0 -D -Uflash:w:./build/arduino.avr.uno/code.ino.hex:i
